7925025f18e8548b2e82b62ab77d0a95139b0b7a
jemartine <jorgeesteban464@gmail.com>
2025-06-02 16:09:48 -0500

    primera aproximación

--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,245 @@
 import sys
-import tablero
+from Tablero import *
+import heapq
+from collections import deque, OrderedDict
+import copy
 
 
 
-def juego():
+# Direcciones y movimientos
+MOVIMIENTOS = {
+    'w': (-1, 0),
+    's': (1, 0),
+    'a': (0, -1),
+    'd': (0, 1),
+}
+
+DIR_INVERSA = {
+    (-1, 0): 'w',
+    (1, 0): 's',
+    (0, -1): 'a',
+    (0, 1): 'd',
+}
+
+def heuristica(a, b):
+    # Distancia de Manhattan
+    #print(f"Heuristica usada a: {a}, b:{b} = {abs(a[0] - b[0]) + abs(a[1] - b[1])}")
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def encontrar_pares(tablero):
+    #Encuentra las posiciones en la matriz de los valores que deben ser conectados
+    posiciones = {}
+    for i, fila in enumerate(tablero.matriz):
+        for j, val in enumerate(fila):
+            if val > 0:
+                if val not in posiciones:
+                    posiciones[val] = []
+                posiciones[val].append((i, j))
+
+    #return posiciones
+    return ordenar_pares(posiciones)
+
+def ordenar_pares(pares):
+    heapPares=[]
+
+    for dato, coord in pares.items():
+        inicio, fin = coord
+        h = heuristica(inicio, fin)
+
+        heapq.heappush(heapPares, (h, dato, coord))
+    
+    pares_ordenados = []
+    elementos_ordenados = heapq.nsmallest(len(heapPares), heapPares)
+
+    for elemento in elementos_ordenados:
+        h, dato, coord = elemento
+        pares_ordenados.append((dato, coord))
+
+
+    #print(f"Pares ordenados {pares_ordenados}")
+    return pares_ordenados
+
+def revisar_salidas(dat, tablero, coord):
+    f, c = coord
+    fila = len(tablero)
+    colu = len(tablero[0])
+    bloqueados = []
+    libre =0
+    bloquedos_aux=[]
+
+    for df, dc in MOVIMIENTOS.values():
+        nf, nc = f + df, c + dc
+
+        if (0 <= nf < fila and 0 <= nc < colu):# revisar limites
+            dato_revisar = tablero[nf][nc]
+            if dato_revisar == 0:
+                libre += 1
+            if dato_revisar < 0 and dato_revisar != -dat:  # solo bloquea si es otro dato
+                bloquedos_aux.append((abs(dato_revisar), (nf, nc)))
+
+    bloqueados.append((bloquedos_aux, libre))
+    return bloqueados
+
+
+def astar(tablero_original, dato, pares, bloqueados,  inicio, fin):
+    heap = [] #lista abierta donde almaceno nuevos nodos                   
+    heapq.heappush(heap, (0 + heuristica(inicio, fin), 0, inicio, [])) # f, g , nodo_inicial, lista de movimientos
+    visitados = set() #Lista con las posiciones visitadas (reconstruye el camino final)
+
+    while heap:
+        f, g, actual, camino = heapq.heappop(heap)
+        #Punto (f, c) actual que estoy revisando
+        if actual in visitados:
+            continue
+        visitados.add(actual)
+
+        #El nodo que estoy revisando, es el nodo objetivo
+        if actual == fin:
+            return camino  #los movimientos necesarios para llegar del nodo inicial al final
+
+        
+        #revisar vecinos
+        for delta in MOVIMIENTOS.values():
+            
+            nueva_pos = (actual[0] + delta[0], actual[1] + delta[1])
+
+            #Dentro de los límites del tablero, no lo haya visitado y no esté bloqueado
+            if (0 <= nueva_pos[0] < len(tablero_original) and
+                0 <= nueva_pos[1] < len(tablero_original[0]) and
+                nueva_pos not in visitados and
+                ((dato,nueva_pos)) not in bloqueados):
+
+                celda = tablero_original[nueva_pos[0]][nueva_pos[1]]
+                #Revisar que esté libre la nueva posición o que sea la posición objetivo
+                if celda == 0 or nueva_pos == fin:
+                    #Actualizar costos
+                    nuevo_g = g + 1#Costo de 1 paso al pasar a la nueva posición
+                    nuevo_f = nuevo_g + heuristica(nueva_pos, fin) #nuevo valor del nodo en el que me paro
+                    heapq.heappush(heap, (nuevo_f, nuevo_g, nueva_pos, camino + [delta]))
+
+
+            
+    return None  # No se encontró camino
+
+#Revvisar que en cada iteración, al menos cada número que deba ser conectado esté libre a su al rededor
+
+def salidas_pares(bloqueados, copia, pares):
+    for dat, (inicio, fin) in copy.deepcopy(pares):
+        for punto in [inicio, fin]:
+            bloqueos = revisar_salidas(dat, copia.matriz, punto)
+            #print(f"Dato {dat}, punto {punto}: {bloqueos}")
+
+            tupla_bloqueo = bloquear_menor_heuristica(fin, bloqueos)
+
+            if tupla_bloqueo is not None and not copia.esta_conectado(dat):
+                bloqueados.append(tupla_bloqueo)
+                mover_un_paso_atras(pares, tupla_bloqueo[0])
+                return True
+    return False
+
+def bloquear_menor_heuristica(fin, bloqueos):
+    for bloqueos_aux, libre in bloqueos:
+        if libre == 0:
+            menor = None
+            min_heur = float('inf')
+
+            for dato, punto in bloqueos_aux:
+                #bloquear la menor heuristica
+                h = heuristica(fin, punto)
+
+                if h<min_heur:
+                    min_heur = h
+                    menor = (dato, punto)
+            return menor
+        
+    return None
+
+def mover_un_paso_atras(pares, dato_objetivo):
+    for i in range(1, len(pares)):  # desde 1 para no mover si ya está al principio
+        if pares[i][0] == dato_objetivo:
+            par = pares.pop(i)
+            pares.insert(i + 1, par)
+            break
+
+def mover_adelante(pares, dato_objetivo):
+    for i in range(1, len(pares)):  # desde 1 para no mover si ya está al principio
+        if pares[i][0] == dato_objetivo:
+            par = pares.pop(i)
+            pares.insert(i + 1, par)
+            break
+
+def jugar(arch_name):
+    tablero_auto = Tablero()
+    tablero_auto.load_table(arch_name)
+
+    tablero_temp=copy.deepcopy(tablero_auto)
+
+    pares = encontrar_pares(tablero_auto)
+
+    print(pares)
+
+    bloqueados=[]
+
+    i=0
+
+    while i<len(pares):
+
+        
+        dato, coords = pares[i]
+        inicio, fin = coords
+        copia = copy.deepcopy(tablero_temp.matriz)
+
+        #recalcular camino
+        camino = astar(copia, dato, pares, bloqueados, inicio, fin)
+
+        print(f"Calculando camino con {dato}")
+
+
+        if not camino:
+            print(f"No se encontró camino para {dato}")
+
+            if i>0:
+                i=0
+                
+                tablero_temp = copy.deepcopy(tablero_auto)
+                print(f"Bloqueados {bloqueados}")
+                continue
+            else:
+                print(f"Primer dato {dato} no tiene camino.")
+                i += 1
+                continue
+                
+        
+
+        # Si hay camino, ejecútalo
+        fila, col = inicio
+        tablero_temp.coordenadas_usdadas.append(inicio)
+
+        for mov in camino:
+            direccion = DIR_INVERSA[mov]
+            _, fila, col = tablero_temp.revisar_moviento(fila, col, direccion, dato)
+        
+        tablero_temp.mostrar_matriz()
+
+        print(f"Se han conectado las casillas de {dato}")
+        tablero_temp.aumentar_conectados(dato, fin[0], fin[1])
+        i += 1  # pasa al siguiente dato
+
+        if salidas_pares(bloqueados, tablero_temp, pares):
+            print(f"Pares: {pares}")
+            print(f"Bloqueados: {bloqueados}")
+            tablero_temp = copy.deepcopy(tablero_auto)
+            i=0
+
+
+        input()
+
+    if tablero_temp.check_terminado():
+        print("¡Jugador sintético ha ganado!")
+
+
+
+def juego(arch_name):
     """
         Controla el flujo del juego principal. Muestra el tablero, recibe las 
         coordenadas del jugador y permite el movimiento de las piezas en el 
@@ -19,27 +255,28 @@ def juego():
         - coordenadas(): para obtener las coordenadas de la celda seleccionada.
         - revisar_moviento(): para verificar y realizar movimientos en el tablero.
     """
-
-    while not tablero.get_terminado():
-        tablero.mostrar_matriz()
+    tablero_manual = Tablero()
+    tablero_manual.load_table(arch_name)
+    while not tablero_manual.get_terminado():
+        tablero_manual.mostrar_matriz()
         print("Juega tu turno.")
 
-        fila, col = tablero.coordenadas()
-        dato_selec= tablero.dato_seleccionado(fila, col)
+        fila, col = tablero_manual.coordenadas()
+        dato_selec= tablero_manual.dato_seleccionado(fila, col)
         estado = True
 
         while estado:
-            tablero.mostrar_matriz()
+            tablero_manual.mostrar_matriz()
             print("Mover con: a (izq), w (arriba), s (abajo), d (der)")
             
             mov = input(": ").lower()
-            estado, fila, col= tablero.revisar_moviento(fila, col, mov, dato_selec)
+            estado, fila, col= tablero_manual.revisar_moviento(fila, col, mov, dato_selec)
 
             if estado!= True:
                 print(f"Se han conectado las casillas de {dato_selec}")
-                tablero.aumentar_conectados(fila, col)
+                tablero_manual.aumentar_conectados(dato_selec,fila, col)
                 
-                if tablero.check_terminado():
+                if tablero_manual.check_terminado():
                     print("¡Has ganado!")
 
 
@@ -49,6 +286,8 @@ if __name__ == '__main__':
     if len(entrada) == 0:
         print("Recuerde: python interfaz.py <archivo de entrada>")
     else:
-        tablero.load_table(entrada[1])
-        juego()
+        if len(entrada)>2 and entrada[2]=="auto":
+            jugar(entrada[1])
+        else:
+            juego(entrada[1])
     
--- tablero.py
+++ tablero.py
@@ -1,62 +1,64 @@
 from colorama import Fore, Style, init
+import copy
 
 init(autoreset=True)
 
-matriz = []
-coordenadas_usdadas = []
-num_conectados = 0
-num_iniciales = 0
-terminado = False
+class Tablero:
+    colores_neg = {
+        -1: Fore.BLUE,
+        -2: Fore.RED,
+        -3: Fore.GREEN,
+        -4: Fore.CYAN,
+        -5: Fore.MAGENTA,
+        -6: Fore.YELLOW,
+    }
 
-colores_neg = {
-    -1: Fore.BLUE,
-    -2: Fore.RED,
-    -3: Fore.GREEN,
-    -4: Fore.CYAN,
-    -5: Fore.MAGENTA,
-    -6: Fore.YELLOW,
-}
+    colores_pos = {
+        1: Fore.BLUE,
+        2: Fore.RED,
+        3: Fore.GREEN,
+        4: Fore.CYAN,
+        5: Fore.MAGENTA,
+        6: Fore.YELLOW,
+    }
 
-colores_pos = {
-    1: Fore.BLUE,
-    2: Fore.RED,
-    3: Fore.GREEN,
-    4: Fore.CYAN,
-    5: Fore.MAGENTA,
-    6: Fore.YELLOW,
-}
+    def __init__(self):
+        self.matriz = []
+        self.tablero_original = []
+        self.coordenadas_usdadas = []
+        self.num_conectados = []
+        self.num_iniciales = 0
+        self.terminado = False
 
+    def dato_seleccionado(self, fila, col):
+        return self.matriz[fila][col]
 
-def dato_seleccionado(fila, col):
-    return matriz[fila][col]
+    def aumentar_conectados(self, dato, fila, col):
+        self.num_conectados.append(dato)
+        self.coordenadas_usdadas.append((fila, col))
 
-def numero_conectados():
-    return num_conectados
+    def esta_conectado(self, dato):
+        if dato in self.num_conectados:
+            return True
+        return False
 
-def aumentar_conectados(fila, col):
-    global num_conectados
-    num_conectados += 1
-    coordenadas_usdadas.append((fila, col))
+    def get_num_iniciales(self):
+        return self.num_iniciales
 
+    def get_coordenadas_usdadas(self):
+        return self.coordenadas_usdadas
 
-def get_num_iniciales():
-    return num_iniciales
+    def get_terminado(self):
+        return self.terminado
 
-def get_coordenadas_usdadas():
-    return coordenadas_usdadas
+    def check_terminado(self):
+        #print(f"Check terminado :{self.num_conectados}, {self.num_iniciales}, {self.revisar_tablero_vacio()}")
+        if len(self.num_conectados) == self.num_iniciales and self.revisar_tablero_vacio():
+            self.terminado = True 
+        return self.terminado
 
-def get_terminado():
-    return terminado
-
-def check_terminado():
-    global terminado
-    if num_conectados == num_iniciales and revisar_tablero_vacio():
-        terminado = True 
-    return terminado
-
-
-def revisar_moviento(fila, col, dir, dato_selec):
-    """
+    def revisar_moviento(self, fila, col, dir, dato_selec):
+        """
         Revisa si el movimiento en la dirección dada es válido. Si lo es, actualiza la
         matriz con el valor de la celda seleccionada y devuelve True y las nuevas
         coordenadas. Si no lo es, devuelve False y las coordenadas no cambian.
@@ -71,153 +73,120 @@ def revisar_moviento(fila, col, dir, dato_selec):
             tuple: (bool, int, int), donde el primer elemento es un booleano que indica
             si se ha llegado al otro elemento, y los otros dos elementos son las nuevas
             coordenadas
-    """
-    nueva_fila, nueva_col = fila, col
+        """
+        nueva_fila, nueva_col = fila, col
 
-    if dir == "w":
-        nueva_fila -= 1
-    elif dir == "s":
-        nueva_fila += 1
-    elif dir == "a":
-        nueva_col -= 1
-    elif dir == "d":
-        nueva_col += 1
-    else:
-        print("Dirección no válida.")
-        return True, fila, col
-
-    if 0 <= nueva_fila < len(matriz) and 0 <= nueva_col < len(matriz[0]):
-        if matriz[nueva_fila][nueva_col] == 0:
-            matriz[nueva_fila][nueva_col] = -1 * dato_selec
-            return True, nueva_fila, nueva_col
-        elif matriz[nueva_fila][nueva_col] == dato_selec and (nueva_fila, nueva_col) not in coordenadas_usdadas:
-            return False, nueva_fila, nueva_col
-
+        if dir == "w":
+            nueva_fila -= 1
+        elif dir == "s":
+            nueva_fila += 1
+        elif dir == "a":
+            nueva_col -= 1
+        elif dir == "d":
+            nueva_col += 1
         else:
-            print("Movimiento no válido.")
-    else:
-        print("Movimiento fuera de los límites.")
+            print("Dirección no válida.")
+            return True, fila, col
 
-    return True, fila, col
+        if 0 <= nueva_fila < len(self.matriz) and 0 <= nueva_col < len(self.matriz[0]):
+            if self.matriz[nueva_fila][nueva_col] == 0:
+                self.matriz[nueva_fila][nueva_col] = -1 * dato_selec
+                return True, nueva_fila, nueva_col
+            elif self.matriz[nueva_fila][nueva_col] == dato_selec and (nueva_fila, nueva_col) not in self.coordenadas_usdadas:
+                return False, nueva_fila, nueva_col
 
-def mostrar_matriz():
-    """
-        Muestra el tablero de juego actual con los índices de fila y columna.
-
-        La función imprime la matriz del tablero de juego, donde se muestra el valor de cada celda.
-        Las filas y columnas se indexan a partir de 1. Las celdas con valor 0 se muestran como 
-        guiones bajos ('_'), mientras que los valores positivos se muestran con el color correspondiente y
-        si es negativo se muestra un # con el color correspondiente al número negativo.
-    """
-
-    print("\nTablero de juego:")
-
-    # Encabezado de columnas
-    encabezado = "     "  # Espacio para el índice de filas
-    for j in range(len(matriz[0])):
-        encabezado += f"{j+1:^3}"
-    print(encabezado)
-
-    # Contenido de la matriz con índice de fila
-    for i, fila in enumerate(matriz):
-        linea = f"{i+1:^5}"  # Índice de fila centrado
-        for valor in fila:
-            if valor == 0:
-                linea += " _ "
-            elif valor > 0:
-                color = colores_pos.get(valor, Fore.WHITE)
-                linea += f"{color}{valor:^3}{Style.RESET_ALL}"
             else:
-                #Imprime # del color de acuerdo al valor para mostrar la conexión
-                color = colores_neg.get(valor, Fore.WHITE)
-                linea += f"{color} # {Style.RESET_ALL}"
+                print("Movimiento no válido.")
+        else:
+            print("Movimiento fuera de los límites.")
 
-        print(linea)
+        return True, fila, col
+    
+    def coordenadas(self):
+        """
+            Pide al usuario que ingrese las coordenadas de una celda en el tablero de juego.
 
-def coordenadas():
-    """
-        Pide al usuario que ingrese las coordenadas de una celda en el tablero de juego.
+            La función verifica que las coordenadas sean válidas y que la celda no esté vacía ni
+            haya sido utilizada previamente. Si las coordenadas son válidas, devuelve la fila y la
+            columna como enteros. Si no lo son, imprime un mensaje de error y vuelve a pedir las
+            coordenadas.
 
-        La función verifica que las coordenadas sean válidas y que la celda no esté vacía ni
-        haya sido utilizada previamente. Si las coordenadas son válidas, devuelve la fila y la
-        columna como enteros. Si no lo son, imprime un mensaje de error y vuelve a pedir las
-        coordenadas.
+            Retorna:
+                tuple: (int, int), donde el primer elemento es la fila y el segundo es la columna
+        """
+        while True:
+            try:
+                print("Ingresa las coordenadas:")
+                fila = int(input("Fila: ")) - 1
+                col = int(input("Columna: ")) - 1
 
-        Retorna:
-            tuple: (int, int), donde el primer elemento es la fila y el segundo es la columna
-    """
-    while True:
-        try:
-            print("Ingresa las coordenadas:")
-            fila = int(input("Fila: ")) - 1
-            col = int(input("Columna: ")) - 1
+                if (0 <= fila < len(self.matriz) and
+                    0 <= col < len(self.matriz[0])):
 
-            if 0 <= fila < len(matriz) and 0 <= col < len(matriz[0]):
-                if matriz[fila][col] != 0 and (fila, col) not in coordenadas_usdadas:
-                    coordenadas_usdadas.append((fila, col))
-                    return fila, col
+                    if (self.matriz[fila][col] != 0 and
+                       (fila, col) not in self.coordenadas_usdadas):
+                        
+                        self.coordenadas_usdadas.append((fila, col))
+                        return fila, col
+                    else:
+                        print("Esta casilla no es válida.")
                 else:
-                    print("Esta casilla no es válida.")
-            else:
-                print("Coordenadas fuera de rango.")
-        except ValueError:
-            print("Por favor ingresa números válidos.")
+                    print("Coordenadas fuera de rango.")
+            except ValueError:
+                print("Por favor ingresa números válidos.")
 
 
-def load_cero_in_matriz(filas, columnas):
-    """
-        Crea una matriz de ceros con el tamaño indicado por parámetros.
-        La matriz se almacena en la variable global 'matriz'.
-    """
-    for _ in range(filas):
-        fila = []
-        for _ in range(columnas):
-            fila.append(0)
-        matriz.append(fila)
+    def mostrar_matriz(self):
+        print("\nTablero de juego:")
+        encabezado = "     "
+        for j in range(len(self.matriz[0])):
+            encabezado += f"{j+1:^3}"
+        print(encabezado)
 
+        for i, fila in enumerate(self.matriz):
+            linea = f"{i+1:^5}"
+            for valor in fila:
+                if valor == 0:
+                    linea += " _ "
+                elif valor > 0:
+                    color = self.colores_pos.get(valor, Fore.WHITE)
+                    linea += f"{color}{valor:^3}{Style.RESET_ALL}"
+                else:
+                    color = self.colores_neg.get(valor, Fore.WHITE)
+                    linea += f"{color} # {Style.RESET_ALL}"
+            print(linea)
 
-def load_table(arch_name):
-    """
-        El formato del archivo es el siguiente: la primera línea contiene el número de filas y columnas, 
-        separadas por una coma. El resto de las líneas contienen la fila, la columna y el valor de cada 
-        celda del tablero, separados por comas. Los índices de fila y columna se basan en 1.
+    def load_cero_in_matriz(self, filas, columnas):
+        self.matriz = [[0 for _ in range(columnas)] for _ in range(filas)]
 
-        Por ejemplo, un archivo con el siguiente texto crearía un tablero de 3x3 con los valores 1 en la 
-        posición (1,1), 2 en la posición (2,2) y 3 en la posición (3,3):
-        3,3
-        1,1,1
-        2,2,2
-        3,3,3
-    """
-    global num_iniciales
+    def load_table(self, arch_name):
+        with open(arch_name, 'r') as f:
+            linea_dimensiones = f.readline().strip().replace(" ", "")
+            filas = int(linea_dimensiones.split(",")[0])
+            columnas = int(linea_dimensiones.split(",")[1])
+            self.load_cero_in_matriz(filas, columnas)
 
-    with open(arch_name, 'r') as f:
-        linea_dimensiones = f.readline().strip().replace(" ", "")
-        filas = int(linea_dimensiones.split(",")[0])
-        columnas = int(linea_dimensiones.split(",")[1])
-
-        #Inicializar la matrz con ceros 
-        load_cero_in_matriz(filas, columnas)
-
-        for line in f:
+            for line in f:
                 line = line.strip().replace(" ", "")
-                fila = int(line.split(",")[0])-1
-                colu = int(line.split(",")[1])-1
+                fila = int(line.split(",")[0]) - 1
+                colu = int(line.split(",")[1]) - 1
                 data = int(line.split(",")[2])
+                self.matriz[fila][colu] = data
+                self.num_iniciales += 1
+            self.tablero_original = copy.deepcopy(self.matriz)
+            self.num_iniciales /= 2
 
-                matriz[fila][colu] = data
-                num_iniciales += 1
-        num_iniciales /= 2
+    def revisar_tablero_vacio(self):
+        #print(f"matriz: {self.matriz}")
+        for fila in self.matriz:
+            for valor in fila:
+                if valor == 0:
+                    return False
+        return True
 
 
-def revisar_tablero_vacio():
-    """
-        Revisa si el tablero está vacío. Si lo está, imprime un mensaje y devuelve True.
-        Si no lo está, devuelve False.
-    """
-    global terminado
-    for fila in matriz:
-        for valor in fila:
-            if valor == 0:
-                return False
-    return True
+    def reset_estado(self):
+        self.matriz = copy.deepcopy(self.tablero_original)
+        self.coordenadas_usdadas = []
+        self.num_conectados = 0
+        self.terminado = False
